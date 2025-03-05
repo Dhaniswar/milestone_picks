5456bd58205beb2c478f38f08b8ed7524dd0151e
Dhaniswar <bkdhaniswar7991@gmail.com>
2025-03-05 21:27:40 +0545

    configure postgresql configuration

--- milestone_picks/settings.py
+++ milestone_picks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,9 +88,14 @@ WSGI_APPLICATION = 'milestone_picks.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["DBNAME"],
+        "PORT": os.environ["DBPORT"],
+        "USER": os.environ["DBUSER"],
+        "HOST": os.environ["DBHOST"],
+        "PASSWORD": os.environ["DBPASS"],
+        
     }
 }
 
