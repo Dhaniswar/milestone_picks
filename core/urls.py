72a61a9f9f9ab12a71267439c7554b41e6206fca
Dhaniswar <bkdhaniswar7991@gmail.com>
2025-03-16 01:21:49 +0545

    Refactor the code

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import HeroSectionViewSet, SportCategoryViewSet, ContactUsViewSet
@@ -11,5 +9,3 @@ router.register(r'contact-us', ContactUsViewSet)
 urlpatterns = [
     path('', include(router.urls)),    
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- milestone_picks/urls.py
+++ milestone_picks/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from milestone_picks import settings
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -30,3 +32,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
